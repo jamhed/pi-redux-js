1fa83cf8f1554632e8c2fc311d5d7ca76373c2bb
Roman Galeev <ip@ncom-ufa.ru>
2014-07-07 17:29:38 +0600

    absolute paths

--- conf/requirejs.js
+++ conf/requirejs.js
@@ -1,12 +1,12 @@
 define({
    catchError: true,
    paths: {
-      "lib/URI":        "lib/uri.js/src",
-      "lib/jquery":     "lib/jquery/dist/jquery.min",
-      "lib/jquery-ui":  "lib/jquery-ui/ui/minified/jquery-ui.min",
-      "lib/doT":        "lib/doT/doT",
-      "lib/bootstrap":  "lib/bootstrap/dist/js/bootstrap.min",
-      "lib/markdown":   "lib/markdown/lib/markdown"
+      "lib/URI":        "/lib/uri.js/src",
+      "lib/jquery":     "/lib/jquery/dist/jquery.min",
+      "lib/jquery-ui":  "/lib/jquery-ui/ui/minified/jquery-ui.min",
+      "lib/doT":        "/lib/doT/doT",
+      "lib/bootstrap":  "/lib/bootstrap/dist/js/bootstrap.min",
+      "lib/markdown":   "/lib/markdown/lib/markdown"
    },
    shim: {
       "lib/jquery": {
