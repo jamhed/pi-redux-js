7ed26bbebe9d03b4a9048f89810f30aa197f9ea5
Roman Galeev <ip@ncom-ufa.ru>
2014-08-03 16:59:35 +0600

    move out editors

--- src/Ed/HTML.cs
+++
@@ -1,5 +0,0 @@
-define [ "a/Ed/CodeMirror", "lib/codemirror/mode/htmlmixed/htmlmixed" ], (P) -> class EdHTML extends P
- 
-   init: ->
-      @a.mode = "htmlmixed"
-      super
--- src/Ed/Textile.cs
+++
@@ -1,9 +0,0 @@
-define [ "a/Ed/CodeMirror", "lib/textile" ], (P, Textile) -> class EdTextile extends P
- 
-   init: ->
-      @a.mode = "textile"
-      super
-
-   process: (text) ->
-      @e.html Textile.convert text
-      @rt.pi @e
