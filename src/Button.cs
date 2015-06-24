bad212d91e8787715adcd36b16c5623278a6859d
Roman Galeev <roman@jamhed.tk>
2015-06-25 00:16:44 +0200

    tabs

--- src/Button.cs
+++ src/Button.cs
@@ -1,9 +1,9 @@
 define ["pi/Pi"], (aPi) -> class Button extends aPi
 
-   attr: -> super.concat ["target", "click"]
+	attr: -> super.concat ["target", "click"]
 
-   init: ->
-      @rpc @a.target, @e.data(), (->) if @a.click
-      @e.click (ev) =>
-         ev.preventDefault()
-         @rpc @a.target, [@e.data()], -> 
+	init: ->
+    	@rpc @a.target, @e.data(), (->) if @a.click
+		@e.click (ev) =>
+			ev.preventDefault()
+        	@rpc @a.target, [@e.data()], -> 
