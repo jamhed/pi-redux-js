050aa34035ec47420b7b11f09cb1eee8ae5ea21d
Roman Galeev <roman@jamhed.tk>
2015-08-27 16:28:30 +0200

    fix: include bootstrap js

--- src/init.cs
+++ src/init.cs
@@ -1,3 +1,5 @@
 require ["conf/requirejs"], (Conf) ->
 	require.config Conf
-	require ["pi/processor"], (Processor) -> processor = new Processor
+	require ["pi/processor"], (Processor) ->
+		require ["lib/bootstrap/js/collapse.js"], (Collapse) -> console.log Collapse
+		processor = new Processor
