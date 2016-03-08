e173777404e0f1db0bce459e80cff31498c64928
Roman Galeev <roman@jamhed.tk>
2016-03-08 21:59:47 +0100

    load bootstrap dropdown

--- src/init.cs
+++ src/init.cs
@@ -1,5 +1,6 @@
 require ["conf/requirejs"], (Conf) ->
 	require.config Conf
 	require ["pi/processor"], (Processor) ->
-		require ["lib/bootstrap/js/collapse.js"], (Collapse) -> console.log Collapse
+		require ["lib/bootstrap/js/collapse.js"], -> 
+		require ["lib/bootstrap/js/dropdown.js"], -> 
 		processor = new Processor
