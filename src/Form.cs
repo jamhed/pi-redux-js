2defcc3e23e5af89c4e4eef6c74d3c5ce889a40c
Roman Galeev <roman@jamhed.tk>
2016-03-24 23:16:57 +0100

    new Form

--- src/Form.cs
+++ src/Form.cs
@@ -1,50 +1,60 @@
-define ["pi/Pi", "pi/Promise"], (P, Promise) -> class aForm extends P
+define ["pi/Pi"], (aPi) -> class aForm extends aPi
 
-	attr: -> super.concat ["uri", "target"]
+	attr: -> super.concat ["el", "target"]
 
-	serialize: (inputs) ->
+	serialize: (inputs = $(@a.el)) ->
 		ret = {}
 		for _i in inputs
-			do (i = $ _i) =>
+			do (i = $ _i) ->
 				group = i.data("group") || "form"
 				ret[group] = [] if ! ret[group]
 				if i.attr("type") == "checkbox"
 					if i.prop("checked")
-						ret[group].push { name: i.attr("name"), value: true, class: i.attr("class") }
+						ret[group].push { name: i.attr("name"), value: true }
 					else
-						ret[group].push { name: i.attr("name"), value: false, class: i.attr("class") }
+						ret[group].push { name: i.attr("name"), value: false }
 				else if i.attr("type") == "radio"
 					if i.prop("checked")
 						ret[group].push { name: i.attr("name"), value: i.attr("value"), class: i.attr("class") }
-				else if i.attr("processed")
-					tmp = @rpc_el i, "get", [], (r) -> return r
-					ret[group].push tmp
 				else
-					tmp = i.serializeArray()
+					tmp = name: i.attr("id"), value: i.val()
 					if i.attr "item_map"
 						item_val = i.attr "item_val"
 						ret[group].push { name: i.attr("item_map"), class: i.attr("class"), value: if item_val == "null" then null else item_val }
-
-					ret[group].push tmp[0]
+					
+					ret[group].push tmp
 		return ret
 
-	deserialize: (inputs, data) ->
-		hdata = {}
-		hdata[d.name] = d for d in data
+	deserialize: (data) ->
+		inputs = $(@a.el)
+		hdata = data
 		for _i in inputs
 			do (i = $ _i) ->
-				if i.attr("type") == "checkbox" && hdata[i.attr("name")]
+				if i.attr("type") == "checkbox" && hdata[i.attr("id")]
 					i.attr("checked", 1)
 				else
-					if f = hdata[i.attr "name"]
-						i.val f.value
-						i.attr "item_val", f["item_val"] if f["item_val"]
+					if f = hdata[i.attr "id"]
+						i.val f
 
-	grab: (a) -> return @serialize $(a.selector)
+	reset: (inputs = $(@a.el)) ->
+		for _i in inputs
+			do (i = $ _i) ->
+				if i.attr("type") == "checkbox"
+					i.removeAttr "checked"
+				else
+					if i.attr "item_map"
+						i.attr "item_val", ""
+					i.val "" 
 
-	send: (a) ->
-		data = @serialize $(a.selector)
-		return @ppost(@a.uri, data).then (r) =>
-			@rpc @a.target, r if @a.target
-			@rpc a.target, r if a.target
-			return r
+	init: ->
+		super
+		@e.click (ev) => @onClick ev
+
+	save: ->
+		data = @serialize()
+		@rpc @a.target, [data.form], ->
+			
+	onClick: (ev) ->
+		ev.preventDefault()
+		data = @serialize()
+		@rpc @a.target, [data.form], ->
